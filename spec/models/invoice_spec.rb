f4a7e57d107d4f0355716f2ca5e0313aa758bff2
Pawel Borak <borak.pawel@gmail.com>
2019-06-16 01:58:48 +0200

    Add specs for invoice model

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Invoice, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {build(:invoice)}
+
+  describe 'validations' do
+    it { should validate_presence_of(:amount) }
+    it { should validate_numericality_of(:amount).is_greater_than(0) }
+    it { should validate_presence_of(:company) }
+    it { should validate_presence_of(:contragent) }
+    it { should validate_presence_of(:currency) }
+    it { should validate_presence_of(:date) }
+  end
+
 end
