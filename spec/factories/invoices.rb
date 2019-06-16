af88c95eb4788bbedb40ea8ec8d003dbe0ce345f
Pawel Borak <borak.pawel@gmail.com>
2019-06-16 20:38:15 +0200

    Add specs for update and delete invoice

--- spec/factories/invoices.rb
+++ spec/factories/invoices.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :invoice do
-    amount { "9.99" }
-    company { "MyString" }
-    contragent { "MyString" }
-    currency { "MyString" }
-    date { "2019-06-16" }
-    user { nil }
+    amount { "1000.99" }
+    company { "Dummy company" }
+    contragent { "Dummy Contragent" }
+    currency { "$" }
+    date { Date.tomorrow }
+    association :user
   end
 end
